be53e1d34912358e59136c621b753ff369a8d74a
Matheus <matheusdiegoteixeira@gmail.com>
2023-11-13 10:21:46 -0300

    matrizes

--- Aula_05/src/aula_05/Vetores.java
+++ Aula_05/src/aula_05/Vetores.java
@@ -4,39 +4,64 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Vetores {
-	
+
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
 		String[] pessoas = { "Luiza", "Aline", "Samara", "Antonio", "Vinicius", "Vitor" };
 
+		// imprimir nomes do array 'pessoas'
 		for (int indice = 0; indice < pessoas.length; indice++) {
 			System.out.println("Posição : " + indice + " = " + pessoas[indice]);
 		}
-		
+
 		System.out.println("-------------------");
-		
+
+		// declara um array vazio
 		float[] notas = new float[5];
-		
-		/* for (int indice = 0; indice < notas.length; indice ++ ) {
+
+		// recebe um valor para cada posição do array
+		for (int indice = 0; indice < notas.length; indice++) {
 			System.out.println("Digite um valor para a posição [" + indice + "] = ");
 			notas[indice] = sc.nextFloat();
 		}
-		
+
+		// imprime o array, item por item
 		for (int indice = 0; indice < notas.length; indice++) {
 			System.out.println("Posição : " + indice + " = " + notas[indice]);
 		}
 
+		// ordena o array
 		Arrays.sort(notas);
 		System.out.println("-------------------------");
-		
-		 for (int indice = 0; indice < notas.length; indice++) {
+
+		for (int indice = 0; indice < notas.length; indice++) {
 			System.out.println("Posição : " + indice + " = " + notas[indice]);
+		}
+
+		System.out.println("-------------------------");
+
+		// localiza item no array
+		System.out.println("Localize item no array : " + Arrays.binarySearch(pessoas, "Aline"));
+
+		for (int indice = 0; indice < pessoas.length; indice++) {
+			if (pessoas[indice].equalsIgnoreCase("Aline")) {
+				System.out.println("Aline está na posição " + indice);
 			}
-		*/
-		
-		System.out.print("Localize 'Aline' no array : " + Arrays.binarySearch(pessoas, "Vinicius"));
+		}
+		// ForEach
+		for (String pessoa : pessoas) {
+			System.out.println(pessoa);
+		}
+
+		int[][] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				System.out.println("Matriz[" + i + "][" + j + "] = " + matriz[i][j]);
+			}
+		}
 
 	}
 
